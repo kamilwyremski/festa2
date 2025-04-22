e07e03c7876499bffab617f30bf8dd58c0695ee6
Kamil Wyremski <kamil.wyremski@gmail.com>
2025-04-22 13:31:17 +0700

    Update to 2.3.4

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /************************************************************************
- * The script of accommodation FESTA 2.3.3
+ * The script of accommodation FESTA 2.3.4
  * Copyright (c) 2018 - 2023 by IT Works Better https://itworksbetter.net
  * Project by Kamil Wyremski https://wyremski.pl
  *
@@ -8,10 +8,10 @@
  *
  * *********************************************************************
  * THIS SOFTWARE IS LICENSED - YOU CAN MODIFY THESE FILES
- * BUT YOU CAN NOT REMOVE OF ORIGINAL COMMENTS!
- * ACCORDING TO THE LICENSE YOU CAN USE THE SCRIPT ON ONE DOMAIN. DETECTION
- * COPY SCRIPT WILL RESULT IN A HIGH FINANCIAL PENALTY AND WITHDRAWAL
- * LICENSE THE SCRIPT
+ * BUT YOU CAN NOT REMOVE OF ORIGINAL COMMENTS!
+ * ACCORDING TO THE LICENSE YOU CAN USE THE SCRIPT ON ONE DOMAIN. DETECTION
+ * COPY SCRIPT WILL RESULT IN A HIGH FINANCIAL PENALTY AND WITHDRAWAL
+ * LICENSE THE SCRIPT
  * *********************************************************************/
 
 session_start();
@@ -21,9 +21,9 @@ header('X-Frame-Options: SAMEORIGIN');
 
 require_once('config/config.php');
 
-$loader = new \Twig\Loader\FilesystemLoader('views/'.$settings['template']);
+$loader = new \Twig\Loader\FilesystemLoader('views/' . $settings['template']);
 $twig = new \Twig\Environment($loader, [
-    'cache' => 'tmp',
+	'cache' => 'tmp',
 ]);
 
 $twig->addFilter(new \Twig\TwigFilter('trans', 'trans'));
@@ -36,55 +36,58 @@ $user = new user();
 
 $controller = 'index';
 
-class noFoundException extends Exception {}
+class noFoundException extends Exception
+{
+}
 
-try{
+try {
 
-	if(!empty($_GET['path'])){
+	if (!empty($_GET['path'])) {
 
-		if(substr($_GET['path'], -1, 1)=='/'){
-			$_GET['path'] = substr($_GET['path'], 0, strlen($_GET['path'])-1);
+		if (substr($_GET['path'], -1, 1) == '/') {
+			$_GET['path'] = substr($_GET['path'], 0, strlen($_GET['path']) - 1);
 		}
 
 		$controller = '';
 
-		$pos_slash = strpos($_GET['path'],'/');
-		if($pos_slash){
+		$pos_slash = strpos($_GET['path'], '/');
+		if ($pos_slash) {
 			$controller = array_search(substr($_GET['path'], 0, $pos_slash), $links);
-		}else{
+		} else {
 			$controller = array_search($_GET['path'], $links);
 		}
 
-		if(($pos_slash or !$controller) and substr($_GET['path'], $pos_slash+1,strlen($_GET['path']))){
-			if(!$controller){
-				$_GET['slug'] = substr($_GET['path'], $pos_slash,strlen($_GET['path']));
-			}else{
-				$_GET['slug'] = substr($_GET['path'], $pos_slash+1,strlen($_GET['path']));
+		if (($pos_slash or !$controller) and substr($_GET['path'], $pos_slash + 1, strlen($_GET['path']))) {
+			if (!$controller) {
+				$_GET['slug'] = substr($_GET['path'], $pos_slash, strlen($_GET['path']));
+			} else {
+				$_GET['slug'] = substr($_GET['path'], $pos_slash + 1, strlen($_GET['path']));
 			}
-			$pos_dash = strpos($_GET['slug'],'-');
-			if($pos_dash){
+			$pos_dash = strpos($_GET['slug'], '-');
+			if ($pos_dash) {
 				$_GET['id'] = intval(substr($_GET['slug'], 0, $pos_dash));
-				$_GET['slug'] = substr($_GET['slug'], $pos_dash+1, strlen($_GET['slug']));
-				if(!$controller and intval($_GET['id'])>0 and $_GET['slug']){
+				$_GET['slug'] = substr($_GET['slug'], $pos_dash + 1, strlen($_GET['slug']));
+				if (!$controller and intval($_GET['id']) > 0 and $_GET['slug']) {
 					$controller = 'classified';
 				}
 			}
 		}
 
-		if(!$controller and  (
-			substr($_GET['path'], 0, strlen(_PREFIX_STATE_))==_PREFIX_STATE_ or
-			substr($_GET['path'], 0, strlen(_PREFIX_CATEGORY_))==_PREFIX_CATEGORY_ or
-			substr($_GET['path'], 0, strlen(_PREFIX_LOCATION_))==_PREFIX_LOCATION_))
-			{
+		if (
+			!$controller and (
+				substr($_GET['path'], 0, strlen(_PREFIX_STATE_)) == _PREFIX_STATE_ or
+				substr($_GET['path'], 0, strlen(_PREFIX_CATEGORY_)) == _PREFIX_CATEGORY_ or
+				substr($_GET['path'], 0, strlen(_PREFIX_LOCATION_)) == _PREFIX_LOCATION_)
+		) {
 			$controller = 'classifieds';
 		}
 
-		if(!$controller){
+		if (!$controller) {
 			throw new noFoundException();
 		}
 	}
-	require_once('controller/'.$controller.'.php');
-}catch(noFoundException $e){
+	require_once('controller/' . $controller . '.php');
+} catch (noFoundException $e) {
 	require_once('controller/404.php');
 }
 
@@ -97,4 +100,4 @@ $settings['logo_facebook'] = getFullUrl($settings['logo_facebook']);
 
 checkInfo();
 
-echo $twig->render($controller.'.html', array_merge($render_variables, ['settings' => $settings, 'user' => $user->user_data, 'controller' => $controller, 'get' => $_GET]));
+echo $twig->render($controller . '.html', array_merge($render_variables, ['settings' => $settings, 'user' => $user->user_data, 'controller' => $controller, 'get' => $_GET]));
